aa585dd6ff981ac318a514bf09e9543b23552d42
AnyTrofi <anytrofi@gmail.com>
2022-09-09 13:43:38 +0300

    Edit some Errors

--- TG.py
+++ TG.py
@@ -2,7 +2,7 @@ import telebot
 from parse import main
 from telebot import types
 
-token = 'Your tg bot token'
+token = '5751633149:AAG1Fi6EXla4Im6ldvpVaVmcd6ngIoY1Bpg'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
@@ -17,16 +17,19 @@ def text(message):
 def get_info(message):
     data = main(message.text)
     if data != 'Error':
-        string = f"PSN ID: {data['name']}\n" \
-                 f"Player level: {data['level']}\n" \
-                 f"Achivment bronze: {data['bronze']}\n" \
-                 f"Achivment silver: {data['silver']}\n" \
-                 f"Achivment gold: {data['gold']}\n" \
-                 f"Achivment platinum: {data['platinum']}\n" \
-                 f"Total achivment: {data['total']}\n" \
-                 f"Total games: {data['games']}"
-        bot.send_photo(message.chat.id, data['avatar'])
-        bot.send_message(message.chat.id, string)
+        try:
+            string = f"PSN ID: {data['name']}\n" \
+                     f"Player level: {data['level']}\n" \
+                     f"Achivment bronze: {data['bronze']}\n" \
+                     f"Achivment silver: {data['silver']}\n" \
+                     f"Achivment gold: {data['gold']}\n" \
+                     f"Achivment platinum: {data['platinum']}\n" \
+                     f"Total achivment: {data['total']}\n" \
+                     f"Total games: {data['games']}"
+            bot.send_photo(message.chat.id, data['avatar'])
+            bot.send_message(message.chat.id, string)
+        except:
+            bot.send_message(message.chat.id, 'Something went wrong')
     else:
         bot.send_message(message.chat.id, 'Wrong ID')
 
--- parse.py
+++ parse.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 # Function with all received data
 def description(soup):
     name = soup.find('span', class_='username').text
     level = int(soup.find('li', class_='icon-sprite level').text)
-    total = int(soup.find('li', class_='total').text)
+    total =  re.sub("\D", "", soup.find('li', class_='total').text)
     platinum = int(soup.find('li', class_='platinum').text)
     gold = int(soup.find('li', class_='gold').text)
     silver = int(soup.find('li', class_='silver').text)
@@ -13,8 +14,6 @@ def description(soup):
     games = (soup.find('span', class_='stat grow').text)[:-12]
     avatar = soup.find('div', class_='avatar').find('img').get('src')
 
-
-    print(avatar)
 
     data = {
         'name': name,
@@ -38,3 +37,5 @@ def main(nickname):
             return data
     except:
         return 'Error'
+
+print(main('x-OCCUPANT-x'))
